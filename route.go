8b212110d616750f7547f254cfe6c482cd4d36b3
Michael <hungnv038@gmail.com>
2015-12-29 18:26:45 +0700

    added source code to route

--- route.go
+++ route.go
@@ -1,1 +1,45 @@
 package gorest
+
+type Route struct {
+	controllers map[string]string
+}
+
+type HttpMethod string
+
+const (
+	HTTP_GET    HttpMethod ="GET"
+	HTTP_POST   HttpMethod ="POST"
+	HTTP_PUT    HttpMethod ="PUT"
+	HTTP_DELETE HttpMethod ="DELETE"
+)
+
+var routeInstance *Route
+
+// Singleton Route instance
+func GetRouteInstance() *Route {
+	if routeInstance == nil {
+		routeInstance = &Route{}
+	}
+	return routeInstance
+}
+
+// add new GET action
+func (route *Route) GET(pattern string, controllerMethod string)  {
+	route.Action(HTTP_GET,pattern,controllerMethod);
+}
+// add new POST action
+func (route *Route) POST(pattern string, controllerMethod string)  {
+	route.Action(HTTP_POST,pattern,controllerMethod);
+}
+// add new PUT action
+func (route *Route) PUT(pattern string, controllerMethod string)  {
+	route.Action(HTTP_PUT,pattern,controllerMethod);
+}
+// add new delete action
+func (route *Route) DELETE(pattern string, controllerMethod string)  {
+	route.Action(HTTP_DELETE,pattern,controllerMethod);
+}
+// perform add new action
+func (route *Route) Action(httpMethod HttpMethod,pattern string, controllerMethod string)  {
+
+}
