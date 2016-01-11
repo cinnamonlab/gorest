c9a3b0b858ba00f28e68ea9ad94ce3d59531c7da
Michael <hungnv038@gmail.com>
2016-01-11 09:57:49 +0700

    updated on application

--- Application.go
+++ Application.go
@@ -1,19 +1,27 @@
 package gorest
+import "net/http"
 
 type Application struct {
 	Config interface{} // the application config
-
+	Route *Route
 }
 
 var application *Application
 
-func SharedApplication()  {
+func SharedApplication() *Application {
 	if application == nil {
 		application = &Application{}
+
+		application.Init()
 	}
 	return application
 }
 
 func (app *Application) Init()  {
+	app.Route = GetRouteInstance()
+}
 
+func (app *Application) StartHttpServer(port int) error  {
+	err := http.ListenAndServe(port,application.Route);
+	return err
 }
